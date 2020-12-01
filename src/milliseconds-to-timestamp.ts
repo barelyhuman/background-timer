1a799bce94b18197df3d25e505f2637e27e00d66
Siddharth Gelera <sidgroks@gmail.com>
2020-12-02 00:51:15 +0530

    avoid type collisions and immutable constants

--- src/milliseconds-to-timestamp.ts
+++ src/milliseconds-to-timestamp.ts
@@ -1,8 +1,8 @@
 export const msTimestamp = (milliseconds) => {
   const asSeconds = milliseconds / 1000;
-  let min: number = Math.floor(asSeconds / 60);
-  let sec: number = Math.floor(asSeconds - min * 60);
-  min = (min < 10 ? '0' + min : min) as number;
-  sec = (sec < 10 ? '0' + sec : sec) as number;
-  return `${min}:${sec}`;
+  const min: number = Math.floor(asSeconds / 60);
+  const sec: number = Math.floor(asSeconds - min * 60);
+  const minString = (min < 10 ? `0${min}` : min) as string;
+  const secString = (sec < 10 ? `0${sec}` : sec) as string;
+  return `${minString}:${secString}`;
 };
